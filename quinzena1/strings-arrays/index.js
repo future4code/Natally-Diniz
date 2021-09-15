9c3279c58b44f3fe7371f2234f96fae227e30ac4
NatallyDiniz <natallydiniz1512@gmail.com>
2021-09-14 22:44:10 -0300

    Exércicio arrays incompleto

---
+++ quinzena1/strings-arrays/index.js
@@ -0,0 +1,47 @@
+// 1 )Indique todas as mensagens impressas no console, SEM EXECUTAR o programa.
+// let array
+// console.log('a. ', array)
+
+//RESPOSTA : a. undefined 
+
+// array = null
+// console.log('b. ', array)
+
+//RESPOSTA : b. null
+
+// array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+// console.log('c. ', array.length) 
+
+//RESPOSTA: c.  11
+
+// let i = 0
+// console.log('d. ', array[i])
+
+//RESPOSTA: d. 11
+
+// array[i+1] = 19
+// console.log('e. ', array)
+
+//RESPOSTA: e. 19
+
+// const valor = array[i+6]
+// console.log('f. ', valor)
+
+//RESPOSTA: f.  i+6
+
+
+// 2)Leia o código abaixo com atenção
+// const frase = prompt("Digite uma frase")
+
+// console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)
+
+//Qual será o valor impresso no console se a entrada do usuário for: "Subi num ônibus em Marrocos"?
+
+//RESPOSTA: SUBI NUM ÔNIBUS EM MIRROCOS , 27
+
+//EXERCÍCIO ESCRITA DE CÓDIGO 
+//1) Faça um programa que pergunte ao usuário seu nome e seu e-mail. Em seguida, imprima na tela a seguinte mensagem:
+//Faça um programa que pergunte ao usuário seu nome e seu e-mail. Em seguida, imprima na tela a seguinte mensagem:
+//O e-mail emailDoUsuario foi cadastrado com sucesso. Seja bem-vinda(o), nomeDoUsuario!
+
+// RESPOSTA:
